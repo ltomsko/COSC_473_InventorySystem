bee88612b08a6042c8546dd12f8ea7944e7fbc5f
cas260 <jhzx@iup.edu>
2019-11-04 17:07:19 -0500

    Iteration1.java

---
+++ Iteration1.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Iteration1 {
+	public static void main(String[] args) {
+	
+	}
+	
+	public static void Window() {
+		LoginGUI window = new LoginGUI();
+
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(500, 500);
+		window.setVisible(true);
+
+	}
+}
