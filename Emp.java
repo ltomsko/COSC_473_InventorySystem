1a700d0f1aaab45587bd2c00dc03836ba2e66b27
cas266 <JHZX@iup.edu>
2019-11-06 09:43:40 -0500

    Emp

---
+++ Emp.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Emp {
+
+	public static void main(String[] args) {
+		EmployeeLandingPage window = new EmployeeLandingPage();
+
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(400, 380);
+		window.setVisible(true);
+
+	}
+
+}
